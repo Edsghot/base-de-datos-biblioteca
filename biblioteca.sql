fa78c4080ca0f7b21da1225474a77717358beaf6
Erick <edsghot@gmail.com>
2022-09-27 23:18:16 -0500

    datos para agregar restricciones

--- biblioteca.sql
+++ biblioteca.sql
@@ -6,74 +6,77 @@ create table Autor(
 	id Integer,
 	autor varchar(100),
 	imagen varchar (100),
-	estado Integer
+	estado Integer -- boolean
 )
+
+-- id autoincremnta
+-- que sea pura letras
 
 create table Configuracion(
 	id_c integer, 
-	nombre varchar(60),
-	telefono varchar(10),
-	direccion varchar(60),
-	correo varchar(80)
+	nombre varchar(60),  -- pura letras
+	celular varchar(10), --9 digitos -- puro numero
+	direccion varchar(60), 
+	correo varchar(80)  -- que contenga @
 )
 
 
 create table Editorial(
-	id_e integer,
-	editorial varchar(100),
-	estado integer,
+	id_e integer, 
+	editorial varchar(100), 
+	estado integer, --boolean
 )
 
 create table Estudiante(
 	id integer,
-	codigo varchar(8),
-	DNI varchar(8),
-	nombre varchar(40),
-	apellido varchar(50),
-	carrera varchar(150),
+	codigo char(6), --char 6 digitos 
+	DNI char(8), -- char puro numeros
+	nombre varchar(40), 
+	apellido varchar(50), 
+	carrera varchar(100),
 	direccion varchar(60),
-	telefono varchar(20),
-	estado integer
+	celular char(9), -- numeros    
+	estado integer -- booleano - bit
 )
 
 
 create table materia(
-	id integer,
+	id integer, 
 	materia varchar(80),
-	estado integer
+	estado integer --bit
 )
 
 create table libro(
 	id integer,
 	titulo varchar(100),
 	cantidad integer,
-	id_autor integer,
-	id_editorial integer,
-	id_materia integer,
-	anio_edicion date,
+	id_autor integer, --foreing
+	id_editorial integer, --foreig
+	id_materia integer, --foreing
+	anio_edicion date, 
 	num_pagina integer,
 	descripcion text,
-	imagen varchar(100),
-	estado integer
+	imagen varchar(150),
+	estado integer --default 0
 )
 
 create table Prestamo(
 	id integer,
-	id_estudiante integer,
-	id_libro integer,
+	id_estudiante integer, --foreing
+	id_libro integer, -- foreing
 	fecha_prestamo date,
 	fecha_devolucion date,
 	cantidad integer,
 	observacion varchar(60),
-	estado integer,
+	estado integer, -- boolean
 )
 
 create table Usuarios(
-	id integer,
+	id integer, --primary key
 	usuario varchar(50),
 	nombre varchar(60),
 	clave varchar(50),
-	rol varchar(40),
-	estado integer
+	rol varchar(40), --docente
+	estado integer  
 )
 
